9a42fc7707d9d25adf27e0683b8ff0c608bc2209
kde <kdeun1@naver.com>
2023-10-17 00:33:43 +0900

    5.2.4 엣지와 연결

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory
+    postedBy: User!
+    taggedUsers: [User!]!
   }
 
   type Query {
@@ -31,10 +33,72 @@ const typeDefs = `
   type Mutation {
     postPhoto(input: PostPhotoInput!): Photo!
   }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+  }
 `;
 
 var _id = 0;
-var photos = [];
+var photos = [
+  {
+    id: '1',
+    name: 'name1',
+    description: 'desc1',
+    category: 'ACTION',
+    githubLogin: 'mHattrup1',
+  },
+  {
+    id: '2',
+    name: 'name2',
+    description: 'desc2',
+    category: 'SELFIE',
+    githubLogin: 'mHattrup3',
+  },
+  {
+    id: '3',
+    name: 'name3',
+    description: 'desc3',
+    category: 'LANDSCAPE',
+    githubLogin: 'mHattrup2',
+  },
+];
+var users = [
+  {
+    githubLogin: 'mHattrup1',
+    name: 'Mike Hattrup1',
+  },
+  {
+    githubLogin: 'mHattrup2',
+    name: 'Mike Hattrup2',
+  },
+  {
+    githubLogin: 'mHattrup3',
+    name: 'Mike Hattrup3',
+  },
+];
+var tags = [
+  {
+    photoID: '1',
+    userID: 'mHattrup2',
+  },
+  {
+    photoID: '2',
+    userID: 'mHattrup2',
+  },
+  {
+    photoID: '2',
+    userID: 'mHattrup1',
+  },
+  {
+    photoID: '2',
+    userID: 'mHattrup3',
+  },
+];
 
 const resolvers = {
   Query: {
@@ -56,6 +120,27 @@ const resolvers = {
 
   Photo: {
     url: parant => `http://localhost.com/img/${parant.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubLogin);
+    },
+    taggedUsers: parent => {
+      return tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userId => users.find(u => u.githubLogin === userId));
+    },
+  },
+
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubLogin === parent.githubLogin);
+    },
+    inPhotos: parent => {
+      return tags
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoId => photos.find(p => p.id === photoId));
+    },
   },
 };
 
