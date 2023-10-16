43e227bcda06309112a7941271ee6a2af5a8467f
kde <kdeun1@naver.com>
2023-10-16 23:19:56 +0900

    5.2.3 인풋 & 열거 타입 사용하기

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
-  # 1. Photo 타입 정의를 추가한다.
+  enum PhotoCategory {
+    SELFIE
+    PRTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
+    category: PhotoCategory
   }
 
-  # 2. allPhotos에서 Photo 타입을 반환한다.
   type Query {
     totalPhotos: Int!
     allPhotos: [Photo!]!
   }
 
-  # 3. 뮤테이션에서 새로 게시된 사진을 반환한다.
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTRAIT
+    description: String
+  }
+
   type Mutation {
-    postPhoto(name: String! description: String): Photo!
+    postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
 
-// 1. 고유 ID를 만들기 위해 값을 하나씩 증가시킨 변수이다.
 var _id = 0;
 var photos = [];
 
@@ -33,10 +44,9 @@ const resolvers = {
 
   Mutation: {
     postPhoto(parent, args) {
-      // 2. 새로운 사진을 만들고 id 부여한다.
       var newPhoto = {
         id: _id++,
-        ...args,
+        ...args.input,
       };
       photos.push(newPhoto);
 
@@ -54,7 +64,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// 4. 웹 서버를 구동하기 위해 listen 메소드를 호출한다.
 server
   .listen()
   .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
